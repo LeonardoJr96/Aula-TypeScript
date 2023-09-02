634b58c7a4d2db86af70178f43e5b1b30325f93d
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-02 19:16:57 -0300

    New commit: TypeScript Class

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+type heroi = {
+    name: string;
+    vulgo:string;
+};
+
+function printObjetos(pessoa:heroi) {
+    console.log(pessoa);
+}
+
+printObjetos({
+    name: "Bruce Wayne",
+    vulgo: "Batman",
+});
