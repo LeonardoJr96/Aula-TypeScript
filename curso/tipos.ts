8fbdeb4317ca242ceb6804c1e74a7378a2fb7644
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-03 17:26:08 -0300

    new commit: tips class

---
+++ curso/tipos.ts
@@ -0,0 +1,19 @@
+//tipos primitivos: boolean, number, string
+let on = false; //boolean
+let ligado: boolean = false; //boolean com trava
+
+let nome: string = "Felipe";
+let idade: number = 30
+let altura: number = 1.9;
+
+//null
+let nulo: null = null;
+
+//undefined
+let indefinido: undefined = undefined;
+
+//any
+let retornoView: any = ""; //aceita qualquer coisa e é utilizado quando o valor que retorna não é previsivel
+
+//void
+let retorno: void //utilizado em funções e não cria retorno
