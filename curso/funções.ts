d15b720668dd27837dd17b6abccaf7c150aab350
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-03 18:16:04 -0300

    update commits: functions class

--- curso/funções.ts
+++ curso/funções.ts
@@ -1,4 +1,14 @@
 //*funções
-function addNumber(x,y){
+function addNumber(x: number, y: number): number{
     return x + y;
 }
+
+let soma: number = addNumber(4,7);
+
+console.log(soma);
+
+function addToHello(name: string): string{
+    return `Hello ${name}`;
+}
+
+console.log(addToHello("felipe"));
