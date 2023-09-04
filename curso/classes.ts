21f52845a41a14f16d83d099310777d396daab60
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-04 17:18:43 -0300

    update commit: new subject

--- curso/classes.ts
+++ curso/classes.ts
@@ -4,23 +4,25 @@
     public - onde todos vê
     private - só as classes vê
     protected - classes e subclasses vê
+    readonly - somente leitura, impedi a troca de valor
 */
 class Character {
-    private name?: string;
-    stregth: number;
+    protected name?: string;
+    readonly stregth: number;
     skill: number;
 
-    constructor(stregth: number, skill: number) {
+    constructor(name: string, stregth: number, skill: number) {
+        this.name = name;
         this.stregth = stregth;
         this.skill = skill;
     }
 
-    attack(): void{
+    protected attack(): void{
         console.log(`Attack with ${this.stregth} points`);
     }
 }
 
-const p1 = new Character(10, 98);
+const p1 = new Character("Ryu", 10, 98);
 console.log(p1); //exibe Character { stregth: [valor] , skill: [valor]}
 
 p1.attack(); //exibe "Attack with ${this.stregth} points"
