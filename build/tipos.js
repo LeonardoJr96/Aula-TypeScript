1300e1bccaea9abf199b281daaddf686d72e4776
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-04 17:48:56 -0300

    update commit: move files

---
+++ build/tipos.js
@@ -0,0 +1,27 @@
+"use strict";
+let on = false;
+let ligado = false;
+let nome = "Felipe";
+let idade = 30;
+let altura = 1.9;
+let nulo = null;
+let indefinido = undefined;
+let retornoView = "";
+let retorno;
+let produto = {
+    name: "Felipe",
+    cidade: "sp",
+    idade: 30,
+};
+let meuProduto = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 5,
+};
+let dados = ["Felipe", "ana", "adriana"];
+let dados2 = ["Felipe", "ana", "adriana"];
+let infos = ["Felipe", 30, 30, "adriana"];
+let ex = [];
+let boleto = ["agua conta", 199.90, 3218615616];
+let aniversario = new Date("2022-12-01 05:00");
+console.log(aniversario.toString());
