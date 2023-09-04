a4dc9f29fbad65bf5efa9a11de182e7dcf1ba3d7
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-04 17:56:38 -0300

    create-server node local

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-type heroi = {
-    name: string;
-    vulgo:string;
-};
-
-function printObjetos(pessoa:heroi) {
-    console.log(pessoa);
-}
-
-//isso printa um objeto
-printObjetos({
-    name: "Bruce Wayne",
-    vulgo: "Batman",
-});
+let dado: string = "Leonardo";
+console.log(dado);
