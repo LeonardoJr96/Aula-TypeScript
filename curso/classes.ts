ed9f6028941ee81effeda65939b67f94a577387e
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-04 17:28:35 -0300

    update commit: subclass

--- curso/classes.ts
+++ curso/classes.ts
@@ -22,7 +22,26 @@ class Character {
     }
 }
 
+//Character: superclass
+//Magician: subclass
+class Magician extends Character {
+    magicPoints: number;
+    constructor(
+        name: string,
+        stregth: number, 
+        skill: number, 
+        magicPoints: number){
+        super(name, stregth, skill)
+        this.magicPoints = magicPoints;
+    }
+}
+
+
+
+
 const p1 = new Character("Ryu", 10, 98);
+const p2 = new Magician("Mago", 9, 30, 100)
+
 console.log(p1); //exibe Character { stregth: [valor] , skill: [valor]}
 
 p1.attack(); //exibe "Attack with ${this.stregth} points"
