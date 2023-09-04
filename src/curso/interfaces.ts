66994740c1bffec1a45b090ddd78d4d6f9249c25
LeonardoJr96 <leo70x7000@gmail.com>
2023-09-04 19:38:20 -0300

    update commit

--- src/curso/interfaces.ts
+++ src/curso/interfaces.ts
@@ -7,6 +7,7 @@ type robo = {                       //tipa variaveis
 interface robot2 {                  // tipa classes
     readonly id: number | string;
     name: string;
+    sayHello(): string
 };
 
 const bot: robo = {
@@ -17,7 +18,27 @@ const bot: robo = {
 const bot1: robot2 = {
     id: 1,
     name: "megaman",
+    sayHello: function (): string {
+        throw new Error("Function not implemented.");
+    }
 };
 
 console.log(bot);
 console.log(bot1);
+
+class Pessoa implements robot2 {
+    id: string | number;
+    name: string;
+
+    constructor (id: string | number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+
+    sayHello(): string {
+        return `Hello ${this.name}`
+    }
+}
+
+const p = new Pessoa(1, "Leonardo");
+console.log(p.sayHello());
